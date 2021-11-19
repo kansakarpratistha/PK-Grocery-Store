620b56629935e0c0295da2f3edcc44351446d394
Pratistha <pratistha.k428@gmail.com>
2021-11-19 18:01:36 +0545

    Friday commit

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -4,40 +4,41 @@ import "mapbox-gl/dist/mapbox-gl.css";
 
 const TOKEN = "pk.eyJ1IjoicHJhdGlzdGhhIiwiYSI6ImNrdGN0dDM0NTBnYTYyb21hdnpoeTZzNGcifQ.Kpw1PTL5VVUTbk0sNF5M3g";
 
-const geolocateStyle = {
-  float: "left",
-  margin: "50px",
-  padding: "10px",
-};
+// const geolocateStyle = {
+//   float: "left",
+//   margin: "50px",
+//   padding: "10px",
+// };
 
 const Map = () => {
-  const [viewport, setViewPort] = useState({
-    width: "100%",
-    height: 900,
-    latitude: 38.8951,
-    longitude: -77.0364,
-    zoom: 10,
-  });
+  // const [viewport, setViewPort] = useState({
+  //   width: "100%",
+  //   height: 900,
+  //   latitude: 38.8951,
+  //   longitude: -77.0364,
+  //   zoom: 10,
+  // });
 
-  const _onViewportChange = (viewport) =>
-    setViewPort({ ...viewport, transitionDuration: 3000 });
+  // const _onViewportChange = (viewport) =>
+  //   setViewPort({ ...viewport, transitionDuration: 3000 });
 
   return (
-    <div style={{ margin: "0 auto" }}>
+    <h1>Map</h1>
+    // <div style={{ margin: "0 auto" }}>
 
-      <MapGL
-        {...viewport}
-        mapboxApiAccessToken={TOKEN}
-        mapStyle="mapbox://styles/mapbox/dark-v8"
-        onViewportChange={_onViewportChange}
-      >
-        <GeolocateControl
-          style={geolocateStyle}
-          positionOptions={{ enableHighAccuracy: true }}
-          trackUserLocation={true}
-        />
-      </MapGL>
-    </div>
+    //   <MapGL
+    //     {...viewport}
+    //     mapboxApiAccessToken={TOKEN}
+    //     mapStyle="mapbox://styles/mapbox/dark-v8"
+    //     onViewportChange={_onViewportChange}
+    //   >
+    //     <GeolocateControl
+    //       style={geolocateStyle}
+    //       positionOptions={{ enableHighAccuracy: true }}
+    //       trackUserLocation={true}
+    //     />
+    //   </MapGL>
+    // </div>
   );
 };
 
