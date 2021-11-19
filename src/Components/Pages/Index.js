0c122aef2462ecf17d620dfb563f0c96841021e9
Pratistha <pratistha.k428@gmail.com>
2021-11-19 17:31:10 +0545

    Nov19

--- src/Components/Pages/Index.js
+++ src/Components/Pages/Index.js
@@ -1,28 +1,28 @@
-// import { React, lazy, Suspense } from "react";
+import { React, lazy, Suspense } from "react";
 import BannerLeft from "../BannerLeft";
-// import DynamicTitle from "../DynamicTitle";
-// const TopBrands = lazy(() => import("../TopBrands"));
-// const FreshVegetables = lazy(() => import("../FreshVegetables"));
+import DynamicTitle from "../DynamicTitle";
+const TopBrands = lazy(() => import("../TopBrands"));
+const FreshVegetables = lazy(() => import("../FreshVegetables"));
 
-// const BannerRight = lazy(() => import("../BannerRight"));
-// const BannerBottom = lazy(() => import("../BannerBottom"));
+const BannerRight = lazy(() => import("../BannerRight"));
+const BannerBottom = lazy(() => import("../BannerBottom"));
 
 function Index() {
-  // DynamicTitle("PK Grocery | Index");
+  DynamicTitle("PK Grocery | Index");
   return (
     <>
       <div className="banner">
         <BannerLeft />
-        {/* <Suspense fallback={<div className="loading-el">Loading...</div>}>
+        <Suspense fallback={<div className="loading-el">Loading...</div>}>
           <BannerRight />
-        </Suspense> */}
+        </Suspense>
         <div className="clearfix"></div>
       </div>
-      {/* <Suspense fallback={<div className="loading-el">Loading...</div>}>
+      <Suspense fallback={<div className="loading-el">Loading...</div>}>
         <BannerBottom />
         <TopBrands text="Top Brands" />
         <FreshVegetables />
-      </Suspense> */}
+      </Suspense>
     </>
   );
 }
